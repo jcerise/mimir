09318cb82ce5b4a624ddcf3ae570cbd2ff3d9bec
jcerise <jcerise06@gmail.com>
2016-10-01 22:58:24 -0600

    Initial commit of package Mimir

---
+++ Mimir/mimir.py
@@ -0,0 +1,2 @@
+def print_test_string():
+    print "This a test of the Mimir package!"
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Mimir',
+      version='0.1',
+      description='A simple, command line, note taking utility.',
+      url='',
+      author='Jeremy Cerise',
+      packages=['Mimir'],
+      zip_safe=False)
