f89884d274ddaeef0f24f8f38dfff30f1310392a
jcerise <jcerise06@gmail.com>
2016-10-04 00:30:49 -0600

    Added creation of notes files upon init; added support for calling with no action, resulting in note creation.

--- Mimir/mimir.py
+++ Mimir/mimir.py
@@ -7,29 +7,37 @@ from Mimir.mimir_handler import MimirHandler
 class Actions(Enum):
     init = 1
     delete = 2
+    new = 3
 
 
 @click.command()
-@click.argument('action')
+@click.argument('action', nargs=-1)
 def cli(action=None):
     actions = Actions
     handler = MimirHandler()
-    try:
-        if action in actions.__members__:
-            if action == 'delete':
-                if click.confirm('Are you sure you wish to delete any mimirs found?'):
+
+    # TODO: What about one word notes? Should we just fall through when an action isn't found?
+    if len(action) == 1:
+        action = action[0]
+        try:
+            if action in actions.__members__:
+                if action == 'delete':
+                    if click.confirm('Are you sure you wish to delete any mimirs found?'):
+                        handler.handle(action)
+                else:
                     handler.handle(action)
             else:
-                handler.handle(action)
-        else:
-            raise KeyError('Action not found!')
-    except KeyError as ex:
-        print 'Invalid action supplied'
+                raise KeyError('Action not defined!')
+        except KeyError as ex:
+            print 'Invalid action supplied'
 
-        valid = ''
-        for action in actions:
-            valid += action.name + ' '
+            valid = ''
+            for action in actions:
+                valid += action.name + ' '
 
-        print 'Valid actions: {}'.format(valid)
-        print 'Run \'mimir --help\' for more information'
+            print 'Valid actions: {}'.format(valid)
+            print 'Run \'mimir --help\' for more information'
+    else:
+        # If multiple arguments were passed in, assume no specific action, and create a note instead.
+        handler.handle('new')
 
--- Mimir/mimir_handler.py
+++ Mimir/mimir_handler.py
@@ -3,6 +3,8 @@ import os
 
 import shutil
 
+import datetime
+
 
 class MimirHandler():
 
@@ -19,15 +21,18 @@ class MimirHandler():
         handler = getattr(self, action_handler)
         handler()
 
-    def _init(self):
+    @staticmethod
+    def _init():
         """
-        Initialize a new mimir, creating a .mimir directory and an initial configuration file
+        Initialize a new mimir, creating a .mimir directory, an initial configuration file, and a notes file
         :return:
         """
         working_dir = os.getcwd()
         mimir_dir = working_dir + '/.mimir'
         config_name = '.mimir_config'
         config_location = mimir_dir + '/' + config_name
+        notes_name = 'mimir_notes.txt'
+        notes_location = mimir_dir + '/' + notes_name
 
         print 'Initializing mimir in {}...'.format(working_dir)
         try:
@@ -43,6 +48,12 @@ class MimirHandler():
                 # (.mimir/.mimir_config)
                 with open(config_location, 'w+') as f:
                     json.dump(config, f)
+
+                # Finally, create the actual notes file
+                with open(notes_location, 'w+') as f:
+                    current_time = datetime.date.today()
+                    f.write('{:%Y-%m-%d %H:%M} Mimir initialized.'.format(current_time))
+
                 print 'Successfully created a new mimir at {}'.format(mimir_dir)
             else:
                 print 'A mimir directory already exists at {}! Aborting...'.format(mimir_dir)
@@ -50,7 +61,8 @@ class MimirHandler():
         except OSError as ex:
             print 'Something went wrong during initialization...'
 
-    def _delete(self):
+    @staticmethod
+    def _delete():
         """
         Delete any found mimirs in the given directory.
         :return:
@@ -66,6 +78,14 @@ class MimirHandler():
                 print 'No mimir found!'
         except OSError as ex:
             print 'Something went wrong attempting to delete this mimir...'
+
+    @staticmethod
+    def _new():
+        """
+        Create a new note entry in the mimir_notes.txt file
+        :return:
+        """
+        print "New called"
 
     @staticmethod
     def handler_not_found():
