65ca3590ba088ac47663ce77a1260788124f3dcf
jcerise <jcerise06@gmail.com>
2016-11-03 15:15:15 -0600

    Re-adding rst readme, as its just easier...will auto-generate rst version from markdown in future

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
+from os import path
 from setuptools import setup
 
-long_description = ''
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    long_description = open('README.md').read()
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
 
 setup(name='MimirNotes',
-      version='0.4.7',
+      version='0.4.9',
       description='A simple, command line, note taking utility.',
       long_description=long_description,
       keywords='note terminal command-line journal',
